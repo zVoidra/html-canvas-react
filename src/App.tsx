40fdfffcaea341c8a4ca88f7f8dbb91fd83be93c
Name <zVoidra@outlook.com>
2024-12-01 14:51:08 +0100

    Added Clock to the main page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import "./App.css";
+import Clock from "./components/Clock/Clock";
 import Sol from "./components/Sol/Sol";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="AppContainer">
       <h1>Hello!</h1>
       <Sol />
-    </>
+      <Clock />
+    </div>
   );
 }
 
